5786e1ac49294df81f069e2036ae2d219c4d2a1b
tonytwei <tony.t.wei@gmail.com>
2023-08-11 11:54:09 +1000

    readme, flask working, multithreading

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# air-pollution-tracker
+# Live Air Pollution Tracker
+
+## Overview
+
+This project aims to create a live air pollution tracker using a Raspberry Pi and environmental sensors. The system collects real-time data on temperature, humidity, pressure, gas levels, and particulate matter, displaying the information on a web page.
+
+## Features
+
+- Real-time data collection of environmental parameters.
+- Web-based dashboard to visualize live air quality metrics.
+- Multi-threading for efficient data collection and presentation.
+- JSON data storage for historical analysis.
+
+## Components
+
+- Raspberry Pi
+- Environmental sensors: BME280, PMS5003
+- Python libraries: `enviroplus`, `bme280`, `pms5003`, `flask`
+- HTML/CSS for web dashboard
+
+## How It Works
+
+1. Environmental sensor data (temperature, humidity, pressure, gas levels, particulate matter) is collected using the Raspberry Pi.
+2. The collected data is processed and stored in JSON format.
+3. A Flask web application serves as a dashboard to display the real-time air quality metrics.
+4. Multi-threading is employed to ensure continuous data collection without blocking the web interface.
+
+## Project Status
+
+The project is currently in progress. Real-time data collection and web dashboard functionality have been implemented. Future enhancements include refining the web interface design and adding historical data analysis.
+
+## Setup and Usage
+
+1. Clone this repository to your Raspberry Pi.
+2. Install required Python libraries.
+3. Run the main script to start data collection and the Flask server: `python my_app.py`.
+4. Access the dashboard by opening a web browser and navigating to `http://raspberry_pi_ip:5000`.
+
+## Future Enhancements
+
+- Historical data analysis and visualization.
+- Integration with external APIs for location-based air quality data.
+- Customized alerts based on air quality thresholds.
+
+
+<img src="https://i.imgur.com/hspH5oy.jpg" alt="Project Image" width="50%">
--- src/enviro.py
+++ src/enviro.py
@@ -1,12 +1,31 @@
+#########################################################################
+#########################################################################
+
+# Configuration
+run_background = True
+run_flask = True
+data_read_interval = 5 # in seconds
+
+
+#########################################################################
 # Sensors
 from enviroplus import gas
 from bme280 import BME280
+from pms5003 import PMS5003
 
 # Processing
 import os
 import time
+from flask import Flask, render_template
+import json
+from math import floor
+import threading
+
+
+app = Flask(__name__)
 
 bme280 = BME280()
+pms5003 = PMS5003()
 
 def read_data():
     temperature = bme280.get_temperature()
@@ -14,15 +33,29 @@ def read_data():
     humidity = bme280.get_humidity()
 
     gases = gas.read_all()
+    particles = pms5003.read()
     
+    pm100 = particles.pm_per_1l_air(10.0)
+    pm50 = particles.pm_per_1l_air(5.0)
+    pm25 = particles.pm_per_1l_air(2.5)
+    pm10 = particles.pm_per_1l_air(1.0)
+    pm5 = particles.pm_per_1l_air(0.5)
+    pm3 = particles.pm_per_1l_air(0.3)
     
     curr_data = {
+        'time' : 0,
         'temp' : round(temperature, 1),
         'humi' : round(humidity, 1),
         'pres' : round(pressure, 1),
         'oxi'  : round(gases.oxidising / 1000, 1),
         'red'  : round(gases.reducing / 1000),
-        'nh3'  : round(gases.nh3 / 1000)
+        'nh3'  : round(gases.nh3 / 1000),
+        'pm03' : pm3,
+        'pm05' : pm5,
+        'pm10' : pm10,
+        'pm25' : pm25,
+        'pm50' : pm50,
+        'pm100': pm100,
     }
     return curr_data
 
@@ -30,11 +63,44 @@ def print_data(curr_data):
     for key, value in curr_data.items():
         print(f'{key}: {value}')
 
+def save_data_to_json(curr_data, file_path):
+    with open(file_path, 'w') as json_file:
+        json.dump(curr_data, json_file, indent=4)
 
-while (True):
 
+
+@app.route('/')
+def index():
     data = read_data()
-    print_data(data)
+    return render_template('sensor_data.html', data=data)
 
-    time.sleep(1)
+
+
+# multi threading
+def background(script_dir):
+    while (True):
+        # t = int(floor(time()))
+        data = read_data()
+        print_data(data)
+        file_path = os.path.join(script_dir, 'data.json')  # Absolute file path
+        save_data_to_json(data, file_path)
+        time.sleep(data_read_interval)
+        os.system('clear')
+
+if __name__ == '__main__':
+    script_directory = os.path.dirname(os.path.abspath(__file__))  # Get the script's directory
+    background_thread = threading.Thread(target=background, args=(script_directory,))
+    background_thread.start()
+
+
+if run_flask and __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
+    
+'''
+while (True):
+    data = read_data(t)
+    print_data(data)
+    save_data_to_json(data, 'data.json')
+    time.sleep(data_read_interval)
     os.system('clear')
+'''
